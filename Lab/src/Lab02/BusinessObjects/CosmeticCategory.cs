3e78ace5abfce8e8b20ecf20db56199fbddd0f1b
Nonobeam <nguyenhuuthuc0405@gmail.com>
2025-05-28 16:15:47 +0700

    Init source

---
+++ Lab/src/Lab02/BusinessObjects/CosmeticCategory.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BusinessObjects;
+
+public partial class CosmeticCategory
+{
+    [Key]
+    public string CategoryId { get; set; } = null!;
+
+    public string CategoryName { get; set; } = null!;
+
+    public string UsagePurpose { get; set; } = null!;
+
+    public string FormulationType { get; set; } = null!;
+
+    public virtual ICollection<CosmeticInformation> CosmeticInformations { get; set; } = new List<CosmeticInformation>();
+}
