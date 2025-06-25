d0be3be16f9cbcc7266d6be4a314cca6584a9042
Nonobeam <nguyenhuuthuc0405@gmail.com>
2025-06-25 23:29:45 +0700

    Feat: Add Feedback controller

---
+++ founder-detection/WebAPI/Controllers/FeedbacksController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Model.Models;
+using Service;
+
+namespace WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class FeedbacksController : ControllerBase
+{
+    private readonly IFeedbackService _ifeedbackService;
+
+    public FeedbacksController(IFeedbackService feedbackService)
+    {
+        _ifeedbackService = feedbackService;
+    }
+
+    // GET: api/Feedbacks
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Feedback>>> GetFeedbacks()
+    {
+        return await _ifeedbackService.GetAllAsync();
+    }
+
+    // GET: api/Feedbacks/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Feedback>> GetFeedback(int id)
+    {
+        var feedback = await _ifeedbackService.GetByIdAsync(id);
+
+        if (feedback == null)
+        {
+            return NotFound();
+        }
+
+        return feedback;
+    }
+
+    // PUT: api/Feedbacks/5
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutFeedback(int id, Feedback feedback)
+    {
+        if (id != feedback.FeedbackId)
+        {
+            return BadRequest();
+        }
+
+        await _ifeedbackService.UpdateAsync(feedback);
+
+        return NoContent();
+    }
+
+    // POST: api/Feedbacks
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPost]
+    public async Task<ActionResult<Feedback>> PostFeedback(Feedback feedback)
+    {
+        await _ifeedbackService.CreateAsync(feedback);
+
+        return CreatedAtAction("GetFeedback", new { id = feedback.FeedbackId }, feedback);
+    }
+
+    // DELETE: api/Feedbacks/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteFeedback(int id)
+    {
+        var feedback = await _ifeedbackService.GetByIdAsync(id);
+        if (feedback == null)
+        {
+            return NotFound();
+        }
+
+        await _ifeedbackService.DeleteAsync(feedback);
+
+        return NoContent();
+    }
+}
---
+++ founder-detection/WepApp/Controllers/FeedbacksController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Model.Models;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace WepApp.Controllers
+{
+    public class FeedbacksController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiBaseUrl = "https://localhost:5001/api/feedbacks";
+
+        public FeedbacksController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var feedbacks = await _httpClient.GetFromJsonAsync<List<Feedback>>(_apiBaseUrl);
+            return View(feedbacks);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var feedback = await _httpClient.GetFromJsonAsync<Feedback>($"{_apiBaseUrl}/{id}");
+            if (feedback == null)
+                return NotFound();
+            return View(feedback);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Feedback feedback)
+        {
+            var response = await _httpClient.PostAsJsonAsync(_apiBaseUrl, feedback);
+            if (response.IsSuccessStatusCode)
+                return RedirectToAction(nameof(Index));
+            return View(feedback);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var feedback = await _httpClient.GetFromJsonAsync<Feedback>($"{_apiBaseUrl}/{id}");
+            if (feedback == null)
+                return NotFound();
+            return View(feedback);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Feedback feedback)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"{_apiBaseUrl}/{id}", feedback);
+            if (response.IsSuccessStatusCode)
+                return RedirectToAction(nameof(Index));
+            return View(feedback);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var feedback = await _httpClient.GetFromJsonAsync<Feedback>($"{_apiBaseUrl}/{id}");
+            if (feedback == null)
+                return NotFound();
+            return View(feedback);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"{_apiBaseUrl}/{id}");
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
